580a31718dd1cb8bf35047efd0aa4d154886d1d0
maria beatriz <134522724+mb18vs@users.noreply.github.com>
2023-11-10 12:12:13 -0300

    up

--- script.js
+++ script.js
@@ -1,3 +1,7 @@
 function mudarTexto() {
     document.getElementById("demo").innerHTML = "A Seleção!";
 }
+let nome = "João";
+  let idade = 25;
+
+  alert("Olá, " + nome + "! Você tem " + idade + " anos.");
